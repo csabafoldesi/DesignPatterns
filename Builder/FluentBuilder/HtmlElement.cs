790d66e1ca5d2065e92a5202f12c91bfe2916be4
Csaba Földesi <csabus@gmail.com>
2023-05-11 23:11:41 +0200

    FluentBuilder

---
+++ Builder/FluentBuilder/HtmlElement.cs
@@ -0,0 +1,50 @@
+﻿using System.Text;
+
+namespace Builder.FluentBuilder
+{
+    public class HtmlElement
+    {
+        public string Name { get; set; } = null!;
+        public string Text { get; set; } = null!;
+        public List<HtmlElement> Elements { get; set; } = new List<HtmlElement>();
+
+        private const int indentSize = 2;
+
+        public HtmlElement()
+        {
+        }
+
+        public HtmlElement(string name, string text)
+        {
+            Name = name;
+            Text = text;
+        }
+
+        private string ToStringImpl(int indent)
+        {
+            var sb = new StringBuilder();
+            var i = new string(' ', indentSize * indent);
+            sb.AppendLine($"{i}<{Name}>");
+
+            if (!string.IsNullOrWhiteSpace(Text))
+            {
+                sb.Append(new string(' ', indentSize * (indent + 1)));
+                sb.AppendLine(Text);
+            }
+
+            foreach (var element in Elements)
+            {
+                sb.Append(element.ToStringImpl(indent + 1));
+            }
+
+            sb.AppendLine($"{i}</{Name}>");
+
+            return sb.ToString();
+        }
+
+        public override string ToString()
+        {
+            return ToStringImpl(0);
+        }
+    }
+}
