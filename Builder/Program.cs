9d3003eb30eb22191521dcf00eafbf880d667dc5
Csaba Földesi <csabus@gmail.com>
2023-05-13 12:54:07 +0200

    Builder finished

---
+++ Builder/Program.cs
@@ -0,0 +1,58 @@
+﻿using System.Text;
+using Builder.FacetedBuilder;
+using Builder.FluentBuilder;
+using Builder.FluentBuilderInheritance;
+using Builder.FunctionalBuilder;
+using Builder.StepwiseBuilder;
+
+namespace Builder
+{
+    internal class Program
+    {
+        
+        
+        static void Main(string[] args)
+        {
+            /*
+            // FluentBuilder
+            var builder = new HtmlBuilder("ul");
+            builder
+                .AddChild("li", "Hello")
+                .AddChild("li", "World");
+            Console.WriteLine(builder.ToString());
+
+            // FluentBuilderInheritance
+            var me = Person
+                .New
+                .Called("Csabus")
+                .WorksAsA("Worker")
+                .Build();
+            Console.WriteLine(me);
+
+            // StepwiseBuilder
+            var car = CardBuilder.Create() // ISpecifyCarType
+                .OfType(CarType.Crossover) // ISPecifyWheelSize
+                .WithWheels(18)            // IBuildCar
+                .Build();*/
+
+            //RunTest("FunctionalBuilder");
+            RunTest("FacetedBuilder");
+
+        }
+
+        static void RunTest(string testName)
+        {
+            switch (testName)
+            {
+                case "FunctionalBuilder":
+                    FunctionalBuilderTest.Run();
+                    break;
+                case "FacetedBuilder":
+                    FacetedBuilderTest.Run();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
