b36d4c69e8e32c8201f5ad8a664f314f8ec4c8cc
Csaba Földesi <csabus@gmail.com>
2023-05-16 23:23:59 +0200

    AsynchronousFactory

---
+++ Factories/AsynchronousFactory/AsynchronousFactoryTest.cs
@@ -0,0 +1,27 @@
+﻿namespace Factories.AsynchronousFactory
+{
+    public class Foo
+    {
+        private Foo() { }
+
+        private async Task<Foo> InitAsync()
+        {
+            await Task.Delay(1000);
+            return this;
+        }
+
+        public static Task<Foo> CreateAsync()
+        {
+            var result = new Foo();
+            return result.InitAsync();
+        }
+    }
+
+    public class AsynchronousFactoryTest
+    {
+        public static async void Run()
+        {
+            Foo foo = await Foo.CreateAsync();
+        }
+    }
+}
