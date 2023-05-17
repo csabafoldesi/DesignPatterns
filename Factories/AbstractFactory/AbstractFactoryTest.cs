1773d2c6d960058157ead61258cdd6dc44a7bac4
Csaba Földesi <csabus@gmail.com>
2023-05-17 23:03:38 +0200

    AbstractFactory

---
+++ Factories/AbstractFactory/AbstractFactoryTest.cs
@@ -0,0 +1,80 @@
+﻿namespace Factories.AbstractFactory
+{
+    public interface IHotDrink
+    {
+        void Consume();
+    }
+
+    internal class Tea : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("Someting with tea");
+        }
+    }
+
+    internal class Coffee : IHotDrink
+    {
+        public void Consume()
+        {
+            Console.WriteLine("Something with coffee");
+        }
+    }
+
+    public interface IHotDrinkFactory
+    {
+        IHotDrink Prepare(int amount);
+    }
+
+    internal class TeaFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Put in a tea bag, pour {amount} ml water");
+            return new Tea();
+        }
+    }
+
+    internal class CoffeeFactory : IHotDrinkFactory
+    {
+        public IHotDrink Prepare(int amount)
+        {
+            Console.WriteLine($"Put in some beans, pour {amount} ml water");
+            return new Coffee();
+        }
+    }
+
+    public class HotDrinkMachine
+    {
+        public enum AvailableDrink
+        {
+            Tea,
+            Coffee
+        }
+
+        private Dictionary<AvailableDrink, IHotDrinkFactory> factories = new();
+
+        public HotDrinkMachine()
+        {
+            factories.Add(AvailableDrink.Tea, new TeaFactory());
+            factories.Add(AvailableDrink.Coffee, new CoffeeFactory());
+        }
+
+        public IHotDrink MakeDrink(AvailableDrink drink, int amount)
+        {
+            return factories[drink].Prepare(amount);
+        }
+
+    }
+
+
+    public class AbstractFactoryTest
+    {
+        public static void Run()
+        {
+            var machine = new HotDrinkMachine();
+            var drink = machine.MakeDrink(HotDrinkMachine.AvailableDrink.Tea, 100);
+            drink.Consume();
+        }
+    }
+}
