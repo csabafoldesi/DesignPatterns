f6259890da22f3f1026989863aeace4ab103cdc2
Csaba Földesi <csabus@gmail.com>
2023-05-19 22:50:11 +0200

    FactoryMethod

---
+++ Factories/FactoryMethod/FactoryMethodTest.cs
@@ -0,0 +1,100 @@
+﻿namespace Factories.FactoryMethod
+{
+    public abstract class DiscountService
+    {
+        public abstract int DiscountPercentage { get; }
+
+        public override string ToString() => GetType().Name;
+    }
+
+    public class CountryDiscountService : DiscountService
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountService(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override int DiscountPercentage
+        {
+            get
+            {
+                return _countryIdentifier switch
+                {
+                    "BE" => 20,
+                    _ => 10,
+                };
+            }
+        }
+    }
+
+    public class CodeDiscountService : DiscountService
+    {
+        public readonly Guid _code;
+
+        public CodeDiscountService(Guid code)
+        {
+            _code = code;
+        }
+
+        public override int DiscountPercentage
+        {
+            get => 15;
+        }
+    }
+
+    public abstract class DiscountFactory
+    {
+        public abstract DiscountService CreateDiscountService();
+    }
+
+    public class CountryDiscountFactory : DiscountFactory
+    {
+        private readonly string _countryIdentifier;
+
+        public CountryDiscountFactory(string countryIdentifier)
+        {
+            _countryIdentifier = countryIdentifier;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CountryDiscountService(_countryIdentifier);
+        }
+    }
+
+    public class CodeDiscountFactory : DiscountFactory
+    {
+        private readonly Guid _code;
+
+        public CodeDiscountFactory(Guid code)
+        {
+            _code = code;
+        }
+
+        public override DiscountService CreateDiscountService()
+        {
+            return new CodeDiscountService(_code);
+        }
+    }
+
+    public class FactoryMethodTest
+    {
+        public static void Run()
+        {
+            var factories = new List<DiscountFactory>
+            {
+                new CodeDiscountFactory(Guid.NewGuid()),
+                new CountryDiscountFactory("BE")
+            };
+
+            foreach (var factory in factories)
+            {
+                var discountService = factory.CreateDiscountService();
+                Console.WriteLine($"Percentage {discountService.DiscountPercentage} coming from {discountService}");
+            }
+
+        }
+    }
+}
